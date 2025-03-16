9cb599bedfec1ec5309724cf5bb8220fc6c4d0b0
Bryan Hickey <bjh@bjh.dev>
2025-03-16 20:02:38 +1100

    add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,12 +13,16 @@ const bitterFont = Bitter({
 })
 
 export const metadata: Metadata = {
-  title: 'Bryan J. Hickey — A Disciple of Jesus Christ.',
-  description: 'Proclaiming the truth, beauty and relevance of Jesus.',
+  title:
+    'Bryan J. Hickey — Proclaiming the truth, beauty and relevance of Jesus.',
+  description:
+    'I have the immense privilege of taking a break from bjh.dev to serve Jesus at my church, City on a Hill Geelong, overseeing theological training of interns, and providing pastoral support.',
 
   openGraph: {
-    title: 'Bryan J. Hickey — A Disciple of Jesus Christ.',
-    description: 'Proclaiming the truth, beauty and relevance of Jesus.',
+    title:
+      'Bryan J. Hickey — Proclaiming the truth, beauty and relevance of Jesus.',
+    description:
+      'I have the immense privilege of taking a break from bjh.dev to serve Jesus at my church, City on a Hill Geelong, overseeing theological training of interns, and providing pastoral support.',
     url: 'https://www.bryanjhickey.com',
     type: 'website',
     siteName: 'bryanjhickey.com',
@@ -27,7 +31,7 @@ export const metadata: Metadata = {
         url: '/images/share-icon.png',
         width: 1200,
         height: 630,
-        alt: 'Bryan J. Hickey — A Disciple of Jesus Christ.',
+        alt: 'Bryan smiling at the camera in an office at night.',
       },
     ],
   },

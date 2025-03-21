43eee173111029f38eb541c25949ab016fa9ad50
Bryan Hickey <bjh@bjh.dev>
2025-03-21 14:15:11 +1100

    add post route and components

---
+++ components/Post.tsx
@@ -0,0 +1,37 @@
+// src/components/Post.tsx
+
+import Image from 'next/image'
+import Link from 'next/link'
+import { PortableText } from '@portabletext/react'
+import { urlFor } from '@/sanity/lib/image'
+import { POST_QUERYResult } from '@/sanity.types'
+import { RelatedPosts } from '@/components/RelatedPosts'
+
+export function Post({ post }: { post: NonNullable<POST_QUERYResult> }) {
+  const { _id, _type, title, mainImage, body, relatedPosts } = post
+
+  return (
+    <main className="prose prose-lg container mx-auto p-4">
+      {title ? <h1>{title}</h1> : null}
+      {mainImage?.asset?._ref ? (
+        <Image
+          className="float-left m-0 mr-4 w-1/3 rounded-lg"
+          src={urlFor(mainImage?.asset?._ref).width(300).height(300).url()}
+          width={300}
+          height={300}
+          alt={title ?? ''}
+        />
+      ) : null}
+      {body ? <PortableText value={body} /> : null}
+      {relatedPosts ? (
+        <RelatedPosts
+          relatedPosts={relatedPosts}
+          documentId={_id}
+          documentType={_type}
+        />
+      ) : null}
+      <hr />
+      <Link href="/">&larr; Return home</Link>
+    </main>
+  )
+}

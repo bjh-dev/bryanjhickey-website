8e6fe3340ac3d30ebd838c1841743efbfffe91ec
Bryan Hickey <bjh@bjh.dev>
2025-05-15 15:32:53 +1000

    fix: adjust excerpt text size for better readability on larger screens

--- src/components/templates/Post.tsx
+++ src/components/templates/Post.tsx
@@ -29,7 +29,7 @@ const Post = ({ post }: Props) => {
 
         <div className="text-foreground/70 my-12 border-y py-12 font-serif text-xl leading-normal lg:text-3xl">
           <p className="text-primary mb-2 font-mono text-xs">TL;DR</p>
-          <p className="text-foreground/60 text-xl leading-relaxed">
+          <p className="text-foreground/60 text-xl leading-relaxed lg:text-2xl">
             {post.excerpt}
           </p>
         </div>

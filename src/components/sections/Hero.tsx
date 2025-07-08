885240719068ef2747da9071d70dd79fed59a221
Bryan Hickey <bjh@bjh.dev>
2025-07-08 12:22:50 +1000

    feat: update hero link color

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -44,7 +44,7 @@ export default function Hero({ section }: { section: HeroSection }) {
               <FadeXAnimation xStartValue={-10} delay={1.5} duration={0.75}>
                 <CustomPortableText
                   value={section.content as PortableTextBlock[]}
-                  paragraphStyles="leading-relaxed text-white lg:text-lg [&_a]:hover:text-white [&_a]:hover:decoration-white"
+                  paragraphStyles="leading-relaxed text-white lg:text-lg [&_a]:font-sans [&_a]:font-medium [&_a]:text-[0.9em] [&_a]:text-primary [&_a]:hover:text-white [&_a]:hover:decoration-white"
                 />
               </FadeXAnimation>
             )}

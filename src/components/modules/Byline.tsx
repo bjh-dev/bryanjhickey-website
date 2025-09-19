f8012d6b7cd9a19a9eb7d971786d05cec0e66070
Bryan Hickey <bjh@bjh.dev>
2025-09-19 13:45:37 +1000

    Add edited date for blogs (#53)
    * feat: add edited date for blog post
    
    * docs: Update project metrics and documentation [skip ci]
    
    * fix: remove console log
    
    * fix: fix faulty type assignment
    
    * docs: Update project metrics and documentation [skip ci]
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- src/components/modules/Byline.tsx
+++ src/components/modules/Byline.tsx
@@ -2,10 +2,13 @@ import DateComponent from '@/components/ui/Date'
 import { Badge } from '@/components/ui/badge'
 import Link from 'next/link'
 import ReadTime from '@/components/modules/ReadTime'
-import { PostCardFragmentType } from '@/lib/sanity/queries/fragments/fragment.types'
+import { PostQueryResult } from '@/types/sanity.types'
 
-export default function Byline({ post }: { post: PostCardFragmentType }) {
-  console.log('Post: ', post)
+export default function Byline({
+  post,
+}: {
+  post: NonNullable<PostQueryResult>
+}) {
   return (
     <div className="grid grid-cols-1 items-start justify-between gap-4 md:grid-cols-2">
       <div className="flex flex-col items-start justify-start gap-2 text-sm">
@@ -16,9 +19,9 @@ export default function Byline({ post }: { post: PostCardFragmentType }) {
             First Published: <DateComponent dateString={post.date} />
           </div>
         )}
-        {post.lastEdited && (
+        {post._updatedAt && (
           <div className="text-foreground/50 text-right">
-            Last Edited: <DateComponent dateString={post.lastEdited} />
+            Last Edited: <DateComponent dateString={post._updatedAt} />
           </div>
         )}
       </div>
@@ -26,18 +29,26 @@ export default function Byline({ post }: { post: PostCardFragmentType }) {
       {post.categories && post.categories?.length > 0 && (
         <div className="flex gap-2">
           <div className="flex flex-wrap gap-1">
-            {post.categories.map((category) => (
-              <Badge
-                variant="default"
-                className="px-3 py-1 text-xs transition-all ease-linear hover:scale-110"
-                asChild
-                key={category._id}
-              >
-                <Link href={`/category/${category.slug}`}>
-                  {category.title}
-                </Link>
-              </Badge>
-            ))}
+            {post.categories.map(
+              (category: {
+                _id: string
+                _type: 'category'
+                title: string | null
+                slug: string | null
+                description?: string | null
+              }) => (
+                <Badge
+                  variant="default"
+                  className="px-3 py-1 text-xs transition-all ease-linear hover:scale-110"
+                  asChild
+                  key={category._id}
+                >
+                  <Link href={`/category/${category.slug}`}>
+                    {category.title}
+                  </Link>
+                </Badge>
+              ),
+            )}
           </div>
         </div>
       )}
